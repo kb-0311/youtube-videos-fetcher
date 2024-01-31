b4e4dadd3c460125bb5b8d67574cc8494875728a
Kanishka <kanishka.bansode@gmail.com>
2024-01-31 21:23:01 +0530

    API key rotation in case of failure

--- videoFetching/videoScheduler.py
+++ videoFetching/videoScheduler.py
@@ -1,5 +1,7 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 import requests
+from flask import Flask, abort
+from . import API_KEYS
 
 
 def fetch_latest_videos_periodically():
@@ -8,13 +10,23 @@ def fetch_latest_videos_periodically():
 
     
     def call_video_fetcher():
-        # makes an API request to video fetcher
-        url = 'http://localhost:5000/videos/latest' 
-        response = requests.get(url)
-        if response.status_code == 200:
-            print("Endpoint called successfully")
-        else:
+        # number of API KEYs used to fetch videos
+        k = 1
+        while True:
+            # makes an API request to video fetcher
+            url = 'http://localhost:5000/videos/latest' 
+            response = requests.get(url)
+            if response.status_code == 200:
+                print("Endpoint called successfully")
+                break
+            
+            # exhausted API KEYs 
+            if k==len(API_KEYS):
+                abort(500)
+
+
             print("Error calling endpoint")
+            k+=1
 
     # Add the cron job to the scheduler
     scheduler.add_job(call_video_fetcher, 'interval', seconds=10)  # Run every 10 seconds
